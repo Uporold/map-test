032bb401ef6aeff4c6123b9420d160d5bc2f648e
Uporold <vg.ryndin@gmail.com>
2022-10-06 14:20:45 +0300

    gh pages fix

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: "map-test",
     environment,
-    rootURL: "/",
+    rootURL: "/map-test/",
     locationType: "auto",
     EmberENV: {
       FEATURES: {
